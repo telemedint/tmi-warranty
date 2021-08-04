3fb29035b29f599e1e8525e45f47f08927117236
M-Abd-ElBaset <36784871+M-Abd-ElBaset@users.noreply.github.com>
2021-08-04 18:29:26 +0200

    Navbar menu shown if authenticated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,13 +31,17 @@
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Telemed') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    @if(Auth::check())
+                        
+                    
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
@@ -78,6 +82,7 @@
 
 
                     </ul>
+                    @endif
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
